119912b308388c55f3d1397a75b16893c63a420d
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-12 22:11:07 +0300

    Home work 1. From Lesson 1. There are possible names of provided variables.

---
+++ src/homework/NamesOfVariables.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class NamesOfVariables {
+    public static void main(String[] args) {
+        String = "numOfShipsInPort";  //1. количество кораблей в порту
+        String = "intCounter";  //2. счётчик целых чисел
+        String = "firstNum";  //3. первое число
+        String = "answOnQuest";  //4. ответ на вопрос
+        String = "randNum";  //5. случайное число
+        String = "randNumGen";  //6. генератор случайных чисел
+        String = "carColor";  //7. цвет автомобиля
+        String = "carSpeed";  //8. скорость движения автомобиля
+        String = "userInsertedNum";  //9. введённое пользователем число
+        String = "maxNum";  //10. наибольшее число
+        String = "minNum";  //11. наименьшее число
+        String = "leftBorderOfNumRange";  //12. левая граница диапазона чисел
+        String = "rightBorderOfNumRange";  //13. правая граница диапазона чисел
+        String = "userGuessNum";  //14. загаданное пользователем число
+        String = "possibleAnswer";  //15. вариант ответа
+        String = "bankAcc";  //16. банковский аккаунт
+        String = "billAmount";  //17. количество денег на счёте
+        String = "bankCard";  //18. банковская карточка
+        String = "bikeWheelNum";  //19. количество колёс у велосипеда
+    }
+}
