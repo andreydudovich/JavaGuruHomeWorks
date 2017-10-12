106c90d4848d01e6619671826b97a09ba851390a
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-12 22:54:04 +0300

    Testing git commit from work PC

--- src/homework/Test.java
+++ src/homework/Test.java
@@ -3,6 +3,6 @@ package homework;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World1!");
+        System.out.println("Hello, World2!");
     }
 }
