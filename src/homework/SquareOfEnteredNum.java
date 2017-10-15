14be0573ef2683350360cf34efcc24582791143c
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-15 20:48:10 +0300

    6th home work

---
+++ src/homework/SquareOfEnteredNum.java
@@ -0,0 +1,13 @@
+package homework;
+
+import java.util.Scanner;
+
+public class SquareOfEnteredNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any number you want to square!");
+        int enteredNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Your entered number square is: " + enteredNum * enteredNum);
+        scanner.close();
+    }
+}
