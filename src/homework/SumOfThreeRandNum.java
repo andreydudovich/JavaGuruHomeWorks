0091cd92bad000a6290ce1d65cdb38e204ee4ed6
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-15 21:45:20 +0300

    9th homework

---
+++ src/homework/SumOfThreeRandNum.java
@@ -0,0 +1,21 @@
+package homework;
+
+import java.util.Random;
+
+/*
+Написать консольную программу, которая генерит 3 случайных числа в диапазоне от 0 до 100,
+выводит их на консоль и подсчитывает их сумму.
+ */
+
+public class SumOfThreeRandNum {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randNumOne = random.nextInt(100);
+        int randNumTwo = random.nextInt(100);
+        int randNumThree = random.nextInt(100);
+        System.out.println("1st rand number: " + randNumOne);
+        System.out.println("2nd rand number: " + randNumTwo);
+        System.out.println("3rd rand number: " + randNumThree);
+        System.out.println("The sum of three random numbers is: " + (randNumOne+randNumTwo+randNumThree));
+        }
+    }
