0d7739e57fa6693b41229fbc4edeb96bed9564e6
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-15 20:10:10 +0300

    Possible names of variables. 1 lesson 1st, 2nd homework

--- src/homework/NamesOfVariables.java
+++ src/homework/NamesOfVariables.java
@@ -2,24 +2,24 @@ package homework;
 
 public class NamesOfVariables {
     public static void main(String[] args) {
-        String = "numOfShipsInPort";  //1. количество кораблей в порту
-        String = "intCounter";  //2. счётчик целых чисел
-        String = "firstNum";  //3. первое число
-        String = "answOnQuest";  //4. ответ на вопрос
-        String = "randNum";  //5. случайное число
-        String = "randNumGen";  //6. генератор случайных чисел
-        String = "carColor";  //7. цвет автомобиля
-        String = "carSpeed";  //8. скорость движения автомобиля
-        String = "userInsertedNum";  //9. введённое пользователем число
-        String = "maxNum";  //10. наибольшее число
-        String = "minNum";  //11. наименьшее число
-        String = "leftBorderOfNumRange";  //12. левая граница диапазона чисел
-        String = "rightBorderOfNumRange";  //13. правая граница диапазона чисел
-        String = "userGuessNum";  //14. загаданное пользователем число
-        String = "possibleAnswer";  //15. вариант ответа
-        String = "bankAcc";  //16. банковский аккаунт
-        String = "billAmount";  //17. количество денег на счёте
-        String = "bankCard";  //18. банковская карточка
-        String = "bikeWheelNum";  //19. количество колёс у велосипеда
+        int numOfShipsInPort;  //1. количество кораблей в порту
+        long intCounter;  //2. счётчик целых чисел
+        int firstNum;  //3. первое число
+        String answOnQuest;  //4. ответ на вопрос
+        int randNum;  //5. случайное число
+        String randNumGen;  //6. генератор случайчиных сел
+        String carColor;  //7. цвет автомобиля
+        int carSpeed;  //8. скорость движения автомобиля
+        int userInsertedNum;  //9. введённое пользователем число
+        int maxNum;  //10. наибольшее число
+        int minNum;  //11. наименьшее число
+        int leftBorderOfNumRange;  //12. левая граница диапазона чисел
+        int rightBorderOfNumRange;  //13. правая граница диапазона чисел
+        int userGuessNum;  //14. загаданное пользователем число
+        String possibleAnswer;  //15. вариант ответа
+        String bankAcc;  //16. банковский аккаунт
+        long billAmount;  //17. количество денег на счёте
+        String bankCard;  //18. банковская карточка
+        int bikeWheelNum;  //19. количество колёс у велосипеда
     }
 }
---
+++ src/homework/TypeOfVariables.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class TypeOfVariables {
+    public static void main(String[] args) {
+        int numOfShipsInPort; //1. количество кораблей в порту
+        int intCounter;  //2. счётчик целых чисел
+        int firstIntNum;  //3. первое целое число
+        int randIntNum; //4. случайное целое число
+        int randRealNum;  //5. случайное вещественное число
+        int carSpeed;  //6. скорость движения автомобиля
+        int userInsertedIntNum;  //7. введённое пользователем целое число
+        int maxIntNum;  //8. наибольшее целое число
+        int minIntNum;  //9. наименьшее вещественное число
+        int leftBorderOfIntNumRange;  //10. левая граница диапазона целых чисел
+        int rightBorderOfIntNumRange;  //11. правая граница диапазона целых чисел
+        int userGuessIntNum;  //12. загаданное пользователем целое число
+        long billAmount;  //13. количество денег на счёте
+        int bikeWheelNum;  //14. количество колёс у велосипедa
+    }
+}
