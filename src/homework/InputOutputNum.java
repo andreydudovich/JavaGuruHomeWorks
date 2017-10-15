84aff35056fcf9e5c94eb074efe13b1791bba71c
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-15 20:31:08 +0300

    3rd, 4th home work

---
+++ src/homework/InputOutputNum.java
@@ -0,0 +1,12 @@
+package homework;
+
+import java.util.Scanner;
+
+public class InputOutputNum {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any number!");
+        int enteredNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("You have entered: " + enteredNum);
+    }
+}
