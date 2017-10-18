537d90eac1191525388b899be5a85ef1147f8fce
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-19 00:38:35 +0300

    4.1 Homework

---
+++ src/lv/javaguru/homework/Print100NumbersFor.java
@@ -0,0 +1,12 @@
+package lv.javaguru.homework;
+/*
+Написать консольную программу, которая при помощи цикла for печатает на консоли числа от 0 до 100.
+ */
+
+public class Print100NumbersFor {
+    public static void main (String[] args){
+        for (int i = 1; i < 101; i++){
+            System.out.println(i);
+        }
+    }
+}
