437de6af2075437a512b4bd2fe64ff628dd2fbd2
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-18 23:48:20 +0300

    4.3 Homework

--- src/lv/javaguru/homework/TheBiggestOfThreeEnteredNumbers.java
+++ src/lv/javaguru/homework/TheBiggestOfThreeEnteredNumbers.java
@@ -9,30 +9,29 @@ import java.util.Scanner;
 public class TheBiggestOfThreeEnteredNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please insert number one, A:");
+        System.out.print("Please insert number one, A:");
         int enteredNumberA = Integer.valueOf(scanner.nextLine());
-        System.out.println("Please insert number two, B:");
+        System.out.print("Please insert number two, B:");
         int enteredNumberB = Integer.valueOf(scanner.nextLine());
-        System.out.println("Please insert number three, C:");
+        System.out.print("Please insert number three, C:");
         int enteredNumberC = Integer.valueOf(scanner.nextLine());
 
-        if (enteredNumberA > enteredNumberB) {
-            if (enteredNumberA > enteredNumberC) {
-                System.out.println("The biggest value is A: " + enteredNumberA);
-            } else {
-                System.out.println("The biggest value is C: " + enteredNumberC);
-            }
+        if((enteredNumberA == enteredNumberB) && (enteredNumberB == enteredNumberC)) {
+            System.out.println("All numbers are equals. A == B == C!");
+        } else if((enteredNumberA > enteredNumberB) && (enteredNumberA > enteredNumberC)) {
+            System.out.println("The biggest value is A: " + enteredNumberA);
+        } else if((enteredNumberB > enteredNumberA) && (enteredNumberB > enteredNumberC)) {
+            System.out.println("The biggest value is B: " + enteredNumberB);
+        } else if((enteredNumberB == enteredNumberA) && (enteredNumberB > enteredNumberC)) {
+            System.out.println("The biggest values are B: " + enteredNumberB + " and A:" + enteredNumberC);
+        } else if((enteredNumberA == enteredNumberC) && (enteredNumberA > enteredNumberB)) {
+            System.out.println("The biggest values are A: " + enteredNumberA + " and C:" + enteredNumberC);
+        } else if((enteredNumberB == enteredNumberC) && (enteredNumberB > enteredNumberA)) {
+            System.out.println("The biggest values are B: " + enteredNumberB + " and C:" + enteredNumberC);
+        } else {
+            System.out.println("The biggest value is C: " + enteredNumberC);
         }
-        if (enteredNumberB > enteredNumberA) {
-            if (enteredNumberB > enteredNumberC) {
-                System.out.println("The biggest value is B: " + enteredNumberB);
-            } else {
-                System.out.println("The biggest value is C: " + enteredNumberC);
-            }
-        }
-        if (enteredNumberA == enteredNumberB && enteredNumberB == enteredNumberC){
-            System.out.println("All numbers are equal. A == B == C");
-        }
+
         scanner.close();
     }
 }
