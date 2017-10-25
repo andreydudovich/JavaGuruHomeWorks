1bc5e6f54b99a920a3796d794d22e9c7ca6a60ed
Andrey Dudovich <andrey.dudovich@gmail.com>
2017-10-25 14:32:21 +0300

    GuessGameHard Вот вот

--- src/lv/javaguru/homework/GuessGameHardMinSteps.java
+++ src/lv/javaguru/homework/GuessGameHardMinSteps.java
@@ -21,35 +21,48 @@ public class GuessGameHardMinSteps {
         Scanner scanner = new Scanner(System.in);
         System.out.print("User please guess a number between 1 and 100. Enter it: ");
         int guessNumber = Integer.valueOf(scanner.nextLine());
-        int computerGuessNumber = random.nextInt(100) + 1; //Программа пытается угадать число от 1 до 100 случайным образом.
-        //String userAnswer;
-
-        if ((guessNumber < 1 || guessNumber > 100) && guessNumber !=computerGuessNumber) {
-            System.out.println("User please don't cheat! Enter number between 1 and 100!");
+        int leftBorder = 1;
+        int rightBorder = 100;
+        int computerGuessNumber = (rightBorder - leftBorder) / 2; //smart value :D
+        String userAnswer;
+        if (guessNumber < 1 || guessNumber > 100) {
+            wrongGuessNumberInserted(guessNumber);
         } else {
-            System.out.println("Computer thinks it is number: " + computerGuessNumber);
-            System.out.print("Is your number Less or More: ");
-            String userAnswer = scanner.nextLine();
-            while (guessNumber != computerGuessNumber) {
-                if (userAnswer.equals("Less")) {
-                    System.out.println("Then maybe guess number is: " + computerGuessNumber);
-                    System.out.print("Is your number Less or More? ");
-                    userAnswer = scanner.nextLine();
-                    computerGuessNumber--; //Глупо, я знаю. Андрей, если ты это читаешь, то я еще напишу отдельный метод для этого, более умный. Запушил, так как писал на рабочем компе, а доступ надо дома.
-                } else if (userAnswer.equals("More")) {
-                    System.out.println("Then maybe guess number is: " + computerGuessNumber);
-                    System.out.print("Is your number Less or More? ");
-                    userAnswer = scanner.nextLine();
-                    computerGuessNumber++; //Глупо, я знаю. Андрей, если ты это читаешь, то я еще напишу отдельный метод для этого, более умный. Запушил, так как писал на рабочем компе, а доступ надо дома.
-                } else {
-                    System.out.print("You made an error. Please insert Less or More: ");
-                    userAnswer = scanner.nextLine();
-                }
+            computerGuessNumber(computerGuessNumber);
+            userAnswer = scanner.nextLine();
+        }
+        while ((guessNumber > 1 && guessNumber < 100) && guessNumber != computerGuessNumber) {
+            if (userAnswer.equals("More")) {
+                leftBorder = computerGuessNumber;
+                computerGuessNumber = smartAlgoritmToGetGuessNumber(rightBorder, leftBorder);
+            } else if (userAnswer.equals("Less")) {
+                rightBorder = computerGuessNumber;
+                computerGuessNumber = smartAlgoritmToGetGuessNumber(rightBorder, leftBorder);
             }
+            computerGuessNumber(computerGuessNumber);
+            userAnswer = scanner.nextLine();
         }
-        if (guessNumber == computerGuessNumber) {
-            System.out.println("Computer thinks it is number: " + computerGuessNumber);
-            System.out.println("Game ends. Computer WINs! Guess number is correct: " + computerGuessNumber);
+        if (userAnswer.equals("Equal")) {
+            winningOutput(computerGuessNumber);
         }
+    }
+
+
+    public static int smartAlgoritmToGetGuessNumber(int rightBorder, int leftBorder) {
+        int rangeMiddle = (rightBorder - leftBorder) / 2;
+        int computerGuessNumber = leftBorder + rangeMiddle;
+        return computerGuessNumber;
+    }
+
+    public static void computerGuessNumber(int computerGuessNumber) {
+        System.out.println("Computer thinks it is number: " + computerGuessNumber);
+        System.out.print("Is your number Less, More or Equal: ");
+    }
+
+    public static void winningOutput(int computerGuessNumber) {
+        System.out.println("Game ends. Computer WINs! Guess number is correct: " + computerGuessNumber);
+    }
+    public static void wrongGuessNumberInserted(int guessNumber) {
+        System.out.println("User please don't cheat! Enter number between 1 and 100! The number " + guessNumber + " is wrong.");
     }
 }
