a3100592234cef511e7efc5f6edf230ded8cbcdd
Andrejs Dudovics <adudovics@evolutiongaming.com>
2017-11-22 10:09:39 +0200

    1.1

---
+++ src/lv/javaguru/homework/gravitrips/FieldTest.java
@@ -0,0 +1,19 @@
+package lv.javaguru.homework.gravitrips;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+
+public class FieldTest {
+
+    private Field field;
+    private Mark mark;
+
+    @Before
+    public void init() {
+}
+
+
+}
